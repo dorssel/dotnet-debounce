1b3797ffd8b8f84c0991e24d14ed1cb455ed270e
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2022-02-19 11:08:03 +0100

    Add trimmable

---
+++ TrimmableTest/Program.cs
@@ -0,0 +1,33 @@
+﻿// SPDX-FileCopyrightText: 2022 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Dorssel.Utilities;
+
+[assembly: CLSCompliant(true)]
+[assembly: ExcludeFromCodeCoverage]
+
+namespace TrimmableTest
+{
+    static class Program
+    {
+        static int Main()
+        {
+            using var debouncer = new Debouncer();
+
+            if (debouncer.GetType().Assembly.GetTypes().Select(t => t.Name).Contains("IDebounce"))
+            {
+                Console.Error.WriteLine("error: Not trimmed. Assembly still contains IDebounce.");
+                return 1;
+            }
+            else
+            {
+                Console.Out.WriteLine("Trimming successful. Assembly no longer contains IDebounce.");
+                return 0;
+            }
+        }
+    }
+}
