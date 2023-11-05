ac6adf4e98fd541936f67a207b1f2af49d78ff2d
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2023-11-05 22:27:59 +0100

    Publish test results
    - also fix documentation warnings
    - also add GitHub link in documentation

---
+++ Documentation/template/public/main.js
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: .NET Foundation and Contributors
+// SPDX-FileCopyrightText: 2023 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/dorssel/TestRepo',
+            title: 'GitHub'
+        }
+    ]
+}
