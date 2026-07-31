1bb5d7800058cc7e0b62171d0b2a164407774fb5
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2026-07-31 11:26:24 +0200

    Add initial fuzzing boilerplate

---
+++ FuzzingTests/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// SPDX-FileCopyrightText: 2026 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1812:Internal class is never instantiated", Justification = "We use internal test classes")]
---
+++ FuzzingTests/MSTestSettings.cs
@@ -0,0 +1,6 @@
+﻿// SPDX-FileCopyrightText: 2026 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+[assembly: DiscoverInternals]
